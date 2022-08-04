bc08993a444c6172bd119e127766482393e742d2
Vishnu I P <107356936+ShylajaKU@users.noreply.github.com>
2022-08-04 10:15:24 +0530

    $config['index_page'] = '';

--- application/config/config.php
+++ application/config/config.php
@@ -37,7 +37,8 @@ $config['base_url'] = 'https://kforkalyanam.in';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+// $config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
