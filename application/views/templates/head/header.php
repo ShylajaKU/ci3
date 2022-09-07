a41eac590c03c420c40339ace03a2a0ec739fb6a
Vishnu I P <107356936+ShylajaKU@users.noreply.github.com>
2022-09-07 15:45:42 +0530

    Update header.php

--- application/views/templates/head/header.php
+++ application/views/templates/head/header.php
@@ -30,7 +30,17 @@
 <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css')?>"> 
 <link rel="stylesheet" href="<?= base_url('assets/css/byme.css')?>"> 
 
+<!-- Google tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-234760008-2">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'UA-234760008-2');
+</script>
+<!-- Google tag (gtag.js) - Google Analytics -->
 
 </head>
 <?php
